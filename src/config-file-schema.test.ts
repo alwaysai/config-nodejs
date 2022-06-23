09aa641d13568dfc798d2dd303310fddff047fef
Eric VanBuhler <eric@alwaysai.co>
2022-06-22 22:19:51 -0700

    Add ConfigFileSchema which supports AJV JSON schema

--- src/config-file-schema.test.ts
+++ src/config-file-schema.test.ts
@@ -1,59 +1,67 @@
 import { chmodSync, existsSync, readFileSync, writeFileSync } from 'fs';
 import * as tempy from 'tempy';
-import * as t from 'io-ts';
 
-import { ConfigFile } from './config-file';
+import { ConfigFileSchema } from './config-file-schema';
 import { join } from 'path';
+import { JSONSchemaType } from 'ajv';
 
-const codec = t.intersection([
-  t.type({
-    foo: t.string,
-  }),
-  t.partial({ baz: t.string }),
-]);
+interface TestSchema {
+  foo: string;
+  baz?: string;
+}
+
+const schema: JSONSchemaType<TestSchema> = {
+  type: 'object',
+  properties: {
+    foo: { type: 'string' },
+    baz: { type: 'string', nullable: true },
+  },
+  required: ['foo'],
+  additionalProperties: false,
+};
 
 const path = tempy.file();
 
-const initialValue = {
+const initialValue: TestSchema = {
   foo: 'foo',
 };
 
-const subject = ConfigFile({
+const subject = ConfigFileSchema({
   path,
-  codec,
+  schema,
   initialValue,
 });
 
-describe(ConfigFile.name, () => {
+describe(ConfigFileSchema.name, () => {
   beforeEach(() => {
     subject.remove();
   });
 
-  it('"write" synchronously writes a file to the specified path', () => {
+  test('"write" synchronously writes a file to the specified path', () => {
     expect(existsSync(path)).toBe(false);
     subject.write({ foo: 'foo', baz: undefined });
     expect(existsSync(path)).toBe(true);
   });
 
-  it('"write" returns the file contents as "serialized"', () => {
+  test('"write" returns the file contents as "serialized"', () => {
     const info = subject.write({ foo: 'foo' });
     expect(info.serialized).toEqual(readFileSync(path, 'utf8'));
   });
 
-  it('"write" returns "changed" as `true` if it wrote the file, false otherwise', () => {
+  test('"write" returns "changed" as `true` if it wrote the file, false otherwise', () => {
     let info = subject.write({ foo: '123' });
     expect(info.changed).toEqual(true);
     info = subject.write({ foo: '123' });
     expect(info.changed).toEqual(false);
   });
 
-  it('"read" reads and parses as JSON the specified file path', () => {
+  test('"read" reads and parses as JSON the specified file path', () => {
     writeFileSync(path, '{"foo": "bar"}');
     const config = subject.read();
     expect(config).toEqual({ foo: 'bar' });
   });
 
-  it('"update" updates the contents of the file', () => {
+  test('"update" updates the contents of the file', () => {
     writeFileSync(path, '{"foo": "foo"}');
     subject.update((config) => {
       config.foo = 'bar';
@@ -61,29 +69,31 @@ describe(ConfigFile.name, () => {
     expect(JSON.parse(readFileSync(path, 'utf8'))).toEqual({ foo: 'bar' });
   });
 
-  it('"update" uses the provided default config if the file does not exist', () => {
+  test('"update" uses the provided default config if the file does not exist', () => {
     subject.remove();
     subject.update(() => {});
     expect(JSON.parse(readFileSync(path, 'utf8'))).toEqual(initialValue);
   });
 
-  it('"update" throws if the updater returns a value', () => {
+  test('"update" throws if the updater returns a value', () => {
     expect(() => subject.update(() => ({ foo: 'bar' }))).toThrow('Mutate');
   });
 
-  it('"update" throws if the there is no initial nor current value', () => {
+  test('"update" throws if the there is no initial nor current value', () => {
     expect(() =>
-      ConfigFile({ path: join(path, 'no-initial-value.json'), codec }).update(() => {}),
+      ConfigFileSchema({ path: join(path, 'no-initial-value.json'), schema }).update(
+        () => {},
+      ),
     ).toThrow('ENOENT');
   });
 
-  it('read/write sanity check', () => {
+  test('read/write sanity check', () => {
     const config = { foo: 'bar' };
     subject.write(config);
     expect(subject.read()).toEqual(config);
   });
 
-  it('initialize writes the file if it does not already exist', () => {
+  test('initialize writes the file if it does not already exist', () => {
     const config = { foo: 'bar' };
     subject.write(config);
     subject.initialize();
@@ -93,17 +103,40 @@ describe(ConfigFile.name, () => {
     expect(subject.read()).toEqual(initialValue);
   });
 
-  it('initialize throws if no initial value is provided', () => {
-    const configFile = ConfigFile({ path: join(path, 'no-initial-value-2.json'), codec });
+  test('errors are undefined if validation succeeds', () => {
+    const config = { foo: 'bar' };
+    subject.write(config);
+    expect(subject.getErrors()).toBe(null);
+  });
+
+  test('errors are provided when validation fails', () => {
+    writeFileSync(path, '{"bar": "bar"}');
+    expect(() => subject.read()).toThrow();
+    expect(subject.getErrors()).toEqual([
+      {
+        instancePath: '',
+        keyword: 'required',
+        message: "must have required property 'foo'",
+        params: { missingProperty: 'foo' },
+        schemaPath: '#/required',
+      },
+    ]);
+  });
+
+  test('initialize throws if no initial value is provided', () => {
+    const configFile = ConfigFileSchema({
+      path: join(path, 'no-initial-value-2.json'),
+      schema,
+    });
     expect(configFile.initialize).toThrow('initialValue');
   });
 
-  it('write throws EACCES', () => {
+  test('write throws EACCES', () => {
     const tmpDir = tempy.directory();
     chmodSync(tmpDir, 0o000);
-    const configFile = ConfigFile({
+    const configFile = ConfigFileSchema({
       path: join(tmpDir, 'test.json'),
-      codec,
+      schema,
       initialValue,
       EACCES: { code: 'foo', message: 'bar' },
     });
@@ -111,20 +144,20 @@ describe(ConfigFile.name, () => {
     chmodSync(tmpDir, 0o777);
   });
 
-  it('read throws ENOENT', () => {
-    const configFile = ConfigFile({
+  test('read throws ENOENT', () => {
+    const configFile = ConfigFileSchema({
       path: tempy.file(),
-      codec,
+      schema,
       ENOENT: { code: 'foo', message: 'bar' },
     });
     expect(() => configFile.read()).toThrow('bar');
   });
 
-  it('read throws EACCES', () => {
+  test('read throws EACCES', () => {
     const tmpPath = tempy.file();
-    const configFile = ConfigFile({
+    const configFile = ConfigFileSchema({
       path: tmpPath,
-      codec,
+      schema,
       initialValue,
       EACCES: { code: 'foo', message: 'bar' },
     });
---
+++ src/config-file-schema.ts
@@ -0,0 +1,192 @@
+import { dirname, resolve } from 'path';
+import { readFileSync, writeFileSync, renameSync, unlinkSync, existsSync } from 'fs';
+import { CodedError } from '@carnesen/coded-error';
+import mkdirp = require('mkdirp');
+import Ajv, { JSONSchemaType } from 'ajv';
+
+const ajv = new Ajv();
+
+function parse(serialized: string) {
+  const parsed: any = JSON.parse(serialized);
+  return parsed;
+}
+
+function serialize(config: any) {
+  const serialized = `${JSON.stringify(config, null, 2)}\n`;
+  return serialized;
+}
+
+function RandomString() {
+  return Math.random().toString(36).substring(2);
+}
+
+function isErrnoException(error: any): error is NodeJS.ErrnoException {
+  return (
+    Object.prototype.hasOwnProperty.call(error, 'code') ||
+    Object.prototype.hasOwnProperty.call(error, 'errno')
+  );
+}
+
+export function ConfigFileSchema<T>(opts: {
+  path: string;
+  schema: JSONSchemaType<T>;
+  ENOENT?: {
+    message?: string;
+    code?: any;
+  };
+  EACCES?: {
+    message?: string;
+    code?: any;
+  };
+  initialValue?: T;
+}) {
+  const path = resolve(opts.path);
+  type Config = T;
+  const validate = ajv.compile(opts.schema);
+
+  return {
+    path,
+    read,
+    readIfExists,
+    readRaw,
+    readParsed,
+    write,
+    writeRaw,
+    remove,
+    update,
+    exists,
+    initialize,
+    getErrors,
+  };
+
+  function readRaw() {
+    let serialized: string;
+    try {
+      serialized = readFileSync(path, { encoding: 'utf8' });
+    } catch (ex) {
+      if (isErrnoException(ex)) {
+        if (ex.code === 'ENOENT' && opts.ENOENT) {
+          const message = opts.ENOENT.message || ex.message || 'File not found';
+          const code = opts.ENOENT.code || 'ENOENT';
+          throw new CodedError(message, code);
+        } else if (ex.code === 'EACCES' && opts.EACCES) {
+          const message =
+            opts.EACCES.message || ex.message || 'Permission not granted on file';
+          const code = opts.EACCES.code || 'EACCES';
+          throw new CodedError(message, code);
+        }
+      }
+      throw ex;
+    }
+    return serialized;
+  }
+
+  function writeRaw(serialized: string) {
+    const info = {
+      changed: false,
+    };
+    if (existsSync(path) && serialized === readRaw()) {
+      return info;
+    }
+    info.changed = true;
+    const tmpFilePath = `${path}.${RandomString()}.tmp`;
+    try {
+      mkdirp.sync(dirname(tmpFilePath));
+      writeFileSync(tmpFilePath, serialized);
+    } catch (ex) {
+      if (isErrnoException(ex)) {
+        if (ex.code === 'EACCES' && opts.EACCES) {
+          const message =
+            opts.EACCES.message || ex.message || 'Permission not granted on file';
+          const code = opts.EACCES.code || 'EACCES';
+          throw new CodedError(message, code);
+        }
+      }
+      throw ex;
+    }
+    try {
+      renameSync(tmpFilePath, path);
+    } catch (exception) {
+      try {
+        unlinkSync(tmpFilePath);
+      } finally {
+        throw exception;
+      }
+    }
+    return info;
+  }
+
+  function readParsed() {
+    const serialized = readRaw();
+    const parsed = parse(serialized);
+    return parsed;
+  }
+
+  function read() {
+    const parsed = readParsed();
+    if (!validate(parsed)) {
+      throw new Error(`Validation of ${path} failed!`);
+    }
+    return parsed as Config;
+  }
+
+  function exists() {
+    return existsSync(path);
+  }
+
+  function readIfExists() {
+    const maybeConfig: Config | undefined = exists() ? read() : undefined;
+    return maybeConfig;
+  }
+
+  function write(config: Config) {
+    if (!validate(config)) {
+      throw new Error(`Validation of ${config} failed!`);
+    }
+    const serialized = serialize(config);
+    const info = writeRaw(serialized);
+    return { ...info, serialized };
+  }
+
+  function remove() {
+    const value = {
+      changed: false,
+    };
+    if (existsSync(path)) {
+      value.changed = true;
+      unlinkSync(path);
+    }
+    return value;
+  }
+
+  function initialize() {
+    if (typeof opts.initialValue === 'undefined') {
+      throw new Error('"initialize" can only be called if "initialValue" is provided');
+    }
+    if (!exists()) {
+      write(opts.initialValue);
+    }
+  }
+
+  function update(updater: (config: Config) => void) {
+    let config: Config;
+    if (opts.initialValue) {
+      config = readIfExists() || opts.initialValue;
+    } else {
+      config = read();
+    }
+    const returnValue = updater(config);
+    // This mutates the config object ^^
+    if (typeof returnValue !== 'undefined') {
+      throw new Error(
+        'Updater returned a value. Mutate the passed configuration instead.',
+      );
+    }
+    const info = write(config);
+    return info;
+  }
+
+  function getErrors() {
+    return validate.errors;
+  }
+}
